2f04bd27782d9ab0cfa83292397853ea7b0bbe4e
DandelionDIMN <dandeliondimn@gmail.com>
2018-03-15 23:15:25 +0800

    Generations

---
+++ src/includes.h
@@ -0,0 +1,34 @@
+#pragma once
+#ifndef _SE_INCLUDES_
+#define _SE_INCLUDES_
+
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+#include <fstream>
+#include <deque>
+#include <regex>
+#include "SDL.h"
+#pragma comment(lib,"SDL2.lib")
+#pragma comment(lib,"SDL2main.lib")
+
+namespace Suzu {
+	using std::ifstream;
+	using std::ofstream;
+	using std::string;
+	using std::vector;
+	using std::stack;
+	using std::deque;
+	using std::regex;
+	using std::to_string;
+	using std::stoi;
+	using std::stof;
+	using std::stod;
+	using std::regex_match;
+}
+
+#define _DEV_
+
+#endif // !_SE_INCLUDES_
+
---
+++ src/suzu.cpp
@@ -0,0 +1,11 @@
+#include "parser.h"
+#define SDL_MAIN_HANDLED
+
+int main(int argc, char **argv) {
+
+	Suzu::Util util;
+
+	util.PrintEvents();
+
+	return 0;
+}
