b53927c2375d688ebe8a48027d8897a5c6884729
DandelionDIMN <dandeliondimn@gmail.com>
2018-03-27 18:02:51 +0800

    Update code

--- src/basicutility.h
+++ src/basicutility.h
@@ -1,9 +1,8 @@
 #pragma once
 #include "parser.h"
-#include "windows.h"
+
 
 namespace Entry {
-  using namespace Suzu;
   typedef vector<string>* StrListPtr;
   typedef StrListPtr(*Attachment)(void);
   //from MSDN
@@ -18,6 +17,52 @@ namespace Entry {
     Instance(string name, string path);
     bool GetHealth() const { return health; }
     vector<string> &GetList() { return entrylist; }
+  };
+}
+
+namespace Suzu {
+  class MemoryProvider {
+  private:
+    deque<StrPair> dict;
+    MemoryProvider *parent;
+    typedef deque<StrPair>::iterator MemPtr;
+  public:
+    void create(StrPair unit) {
+      if (unit.IsReadOnly()) {
+        dict.push_front(unit);
+      }
+      else {
+        dict.push_back(unit);
+      }
+    }
+
+    StrPair *find(string name) {
+      StrPair *result = nullptr;
+      for (auto &unit : dict) {
+        if (unit.first == name) {
+          result = &unit;
+          break;
+        }
+      }
+      return result;
+    }
+
+    MemoryProvider &SetParent(MemoryProvider *ptr) {
+      this->parent = ptr;
+      return *this;
+    }
+
+    MemoryProvider *GetParent() const {
+      return parent;
+    }
+
+    MemoryProvider() { parent = nullptr; }
+    bool empty() const { return dict.empty(); }
+    size_t size() const { return dict.size(); }
+    void cleanup() { Util().CleanUpDeque(dict); }
+    bool dispose(string name);
+    string query(string name);
+    string set(string name, string value);
   };
 }
 
