2e4a3b799e27ec6f3900d048aae74033e1d4e01d
SuzuNakamura <dandeliondimn@gmail.com>
2018-04-25 10:20:19 +0800

    corrections

--- src/rscripter.cpp
+++ src/rscripter.cpp
@@ -1,6 +1,6 @@
 //BSD 2 - Clause License
 //
-//Copyright(c) 2017 - 2018, Kagami Nakamura
+//Copyright(c) 2017 - 2018, Suzu Nakamura
 //All rights reserved.
 //
 //Redistribution and use in source and binary forms, with or without
